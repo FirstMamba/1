49bb216e669eb4bbeb05876b7596db753f3025ff
Hunter Banducci <hbanducci@cocc.edu>
2018-11-16 00:05:33 -0800

    trying something else

--- CS_160/Python/p5/example2/sketch.js
+++ CS_160/Python/p5/example2/sketch.js
@@ -9,15 +9,12 @@ function draw() {
     //put drawing code here
     if (mouseIsPressed) {
         fill(0);
-    if (scroll) {
-        rotate(scale);
-    }
+
     }
     else {
         fill(126, 140, 162);
     }
     ellipse(mouseX, mouseY, 250, 250)
     rect(mouseX, mouseY, 125, 125)
-    rect(scrollX, screenY, 30, 30);
-    angle +=0.1;
+    Text("Hello", 27, 90)
 }
